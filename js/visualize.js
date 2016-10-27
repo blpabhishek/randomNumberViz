eee7f9198d5c85c4843eb5b03e24d904b274e4b7
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-10-27 14:21:32 +0530

    Created a basic bar chart along with the line and circles.

--- js/visualize.js
+++ js/visualize.js
@@ -25,7 +25,7 @@ var showData = function(data) {
         .domain([1, data.length])
         .range([0, innerWidth]);
 
-    var xAxis = d3.axisBottom(XScale);
+    var xAxis = d3.axisBottom(XScale).ticks(10);
     svg.select('.xAxis').call(xAxis);
 
     var circle = dataGroup.selectAll('circle').data(data);
@@ -51,6 +51,29 @@ var showData = function(data) {
     dataGroup.append('path')
         .attr('d', line(data))
         .classed('line', true);
+
+
+    //Bar Chart 
+    var chart = d3.select(".chart")
+        .attr("width", innerWidth)
+        .attr("height", innerHeight);  
+
+    var barWidth = innerWidth / (data.length*4);
+
+    var bars = chart.selectAll('rect').data(data);
+
+    bars.enter().append("rect")
+      .attr("x", function(d,i) { return XScale(i+1); })
+      .attr("y", function(d) { return YScale(d); })
+      .attr("height", function(d) { return innerHeight - YScale(d); })
+      .attr("width", barWidth);
+
+    bars
+      .attr("y", function(d) { return YScale(d); })
+      .attr("height", function(d) { return innerHeight - YScale(d); })
+
+    bars.exit().remove();
+
 }
 
 var createChart = function() {
@@ -75,8 +98,11 @@ var createChart = function() {
     svg.append('g')
         .classed('dataGroup', true)
         .attr('transform', translate(margin, margin));
-}
 
+    svg.append('g')
+        .classed('chart', true)
+        .attr('transform', translate(margin, margin));
+}
 
 var load = function() {
     var lineInterval;
@@ -92,7 +118,7 @@ var load = function() {
         lineInterval = setInterval(function() {
         showData(data);
         data = updateData(data);
-        }, 250);
+        }, 500);
         button.textContent = "Pause";
     };
     button.onclick();
